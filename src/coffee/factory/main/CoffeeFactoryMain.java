6689ab3fbc0f3c3586250567596c6f7b4771bc5a
RahmanFiros <rahmanfiros123@gmail.com>
2018-05-01 03:15:26 +0600

    Main created!!

--- src/coffee/factory/main/CoffeeFactoryMain.java
+++ src/coffee/factory/main/CoffeeFactoryMain.java
@@ -9,8 +9,16 @@ interface CoffeeType {
 public class CoffeeFactoryMain {
 
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        CoffeeFactory coffeeFactory = Factory.NewCoffeeFactory();
+        CoffeeType coffeeType1 = coffeeFactory.produceCoffee("Coffee A");
+        coffeeType1.Show();
+        CoffeeType coffeeType2 = coffeeFactory.produceCoffee("Coffee B");
+        coffeeType2.Show();
+        CoffeeType coffeeType3 = coffeeFactory.produceCoffee("Coffee C");
+        coffeeType3.Show();
+        CoffeeType coffeeType4 = coffeeFactory.produceCoffee("Coffee D");
+        coffeeType4.Show();
     }
     
 }
